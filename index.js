5c6aaf257ca8560f8ddebcc07146c4140dfab879
abhinav <vabhinav251@gmail.com>
2024-07-11 15:54:50 +0530

    first commit

---
+++ index.js
@@ -0,0 +1,74 @@
+        // line below is used to print on the console windeow
+       // console.log('Namaste Dunia version4'); 
+
+       // let a = true;
+      //  console.log(a);
+      
+      //  const num = 12;
+        // num = 13;
+
+      //  let lastName = '3';
+      //  console.log(lastName);
+//
+       // lastName = true;
+       // console.log(lastName);
+
+        //console.log(3 > 3);
+
+
+//let age = 17;
+//let status = (age >=18) ? 'Yes Vote' : 'No Vote';
+
+//console.log(status);
+
+// let marks = 90;
+// if(marks >= 90) {
+//         console.log('A');
+// }
+// else if(marks >= 80){
+//         console.log('B');
+
+// }
+// else if(marks >=70) {
+//         console.log('C');
+// }
+// else if(marks >= 60){
+//         console.log('D');
+
+// }
+// else{
+//         console.log('E');
+// }
+
+// let num = 2;
+
+// switch(num){
+//         case 1: console.log('A');
+//         break;
+//         case 2: console.log('B');
+//         break;
+//         case 3: console.log('C');
+//         break;
+//         default: console.log('D');
+// }
+
+
+// for(let i =0; i<5; i=i+1){
+//         console.log(i);
+// }
+
+// let x = 1;
+// while(x <= 5){
+//         console.log(x);
+//         x++;
+// }
+
+
+let y = 1;
+
+do{
+    console.log(y);
+    y++;    
+} while(y < 10);
+
+
